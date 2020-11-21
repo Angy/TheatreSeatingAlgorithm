b18c01f7203294c88070f222b285bfd96efc3d58
Angy <theinnovative@gmail.com>
2020-11-21 12:14:57 +0100

    improve fixtures

--- backend/app/fixtures.py
+++ backend/app/fixtures.py
@@ -35,19 +35,19 @@ seat_fixture2 = {
     "id": 4,
     "seat_number": 4,
     "seat_type": "aisle",
-    "allocated_to": 3
+    "allocated_to": None
 }
 
 seat_fixture3 = {
     "id": 3,
     "seat_number": 3,
     "seat_type": "aisle",
-    "allocated_to": 4
+    "allocated_to": None
 }
 
 row_fixture = {
     "row_number": 6,
-    "row_seat": [seat_fixture, seat_fixture1, seat_fixture2, seat_fixture3]
+    "seats": [seat_fixture, seat_fixture1, seat_fixture2, seat_fixture3]
 }
 
 section_fixture = {
