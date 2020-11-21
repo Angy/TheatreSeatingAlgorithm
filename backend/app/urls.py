001a83f5b8cb3a5b393b542ae350a67e76e5fdf6
Angy <theinnovative@gmail.com>
2020-11-21 17:03:10 +0100

    fix import

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
 
 from app.views import HallView, UserView, RowView, UserGroupView, GetRowView, \
-    SeatsView
+    SeatsView, SectionView
 
 urlpatterns = [
     url('^halls/$', HallView.as_view()),
     url('^rows/$', RowView.as_view(), name='rows'),
     url('^rows/(?P<pk>[0-9A-Z-a-z-]+)/$', GetRowView.as_view()),
     url('^seats/$', SeatsView.as_view(), name='seats'),
-    url('^sections/$', SectionView.as_view()))
+    url('^sections/$', SectionView.as_view()),
     url('^users/$', UserView.as_view()),
     url('^users/(?P<pk>[0-9A-Z-a-z-]+)/$', UserGroupView.as_view()),
 ]
