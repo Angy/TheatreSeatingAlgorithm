8e27fb97c735456ad4297e14c59707010d01bdb6
Angy <theinnovative@gmail.com>
2020-11-21 10:48:16 +0100

    admin

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Row, Seat, Hall, User, Section
+
+
+class SectionInlineAdmin(admin.TabularInline):
+    model = Section.rows.through
+    extra = 0
+    verbose_name = 'Section'
+    verbose_name_plural = 'Sections'
+
+
+class RowInLineAdmin(admin.TabularInline):
+    model = Row.seats.through
+    extra = 0
+    verbose_name = 'Row'
+    verbose_name_plural = 'Rows'
+
+
+@admin.register(Row)
+class RowAdmin(admin.ModelAdmin):
+    model = Row
+    fields = ('row_number', 'seats')
+    filter_horizontal = ('seats', )
+    inlines = (SectionInlineAdmin, )
+
+
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    model = Seat
+    fields = ('seat_type', 'seat_number', 'allocated_to', )
+    inlines = (RowInLineAdmin, )
+
+
+@admin.register(Hall)
+class HallAdmin(admin.ModelAdmin):
+    model = Hall
+    fields = ('name', 'sections', )
+    list_display = ('name', )
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ('user_name', 'rank', )
+    list_display = ('user_name', 'rank', )
+    list_filter = ('rank', )
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    model = Section
+    fields = ('section_name', 'rows', )
+    filter_horizontal = ('rows', )
